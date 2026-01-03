7580663a2e20f60bc48fc52d498bcdaac2a7add0
雷家兴 <leij49456@gmail.com>
2026-01-03 19:50:55 +0800

    chore: add comment & add doxyfile config

--- include/optimize/LoopUnroll.hpp
+++ include/optimize/LoopUnroll.hpp
@@ -8,6 +8,13 @@ public:
   bool run(Function &func, const std::vector<LoopInfo> &loops);
 
 private:
+  /**
+   * @brief try to unroll the given loop
+   *
+   * @param func function containing the loop
+   * @param loop loop to unroll
+   * @return whether unrolling was successful
+   */
   bool tryUnrollLoop(Function &func, const LoopInfo &loop);
   bool isSimpleLoop(const LoopInfo &loop, int &tripCount, Operand &iv,
                     int &step, int &initVal);
